e30efb163684dd587b881703137a150594222133
Will Neal <willwneal@gmail.com>
2022-07-06 18:44:00 -0400

    reduce data examples

--- high-level-functions/reduce.js
+++ high-level-functions/reduce.js
@@ -5,7 +5,9 @@ const singleValue = multipleValues.reduce((a, b) => a += b);
 //basic sum with reduce
 //Arrow function syntax: reduce((previousValue, currentValue) => { /* ... */ } )
 const itemPrices = [29.99, 19.95, 54.95, 99.99, 16.35, 10.00]
+
 const cartTotal = itemPrices.reduce((total, itemPrice) => total += itemPrice);
+
 const itemAverage = itemPrices.reduce((total, itemPrice, itemIndex, itemArray) => {
     total += itemPrice
     if (itemIndex === itemArray.length-1) {
@@ -14,6 +16,7 @@ const itemAverage = itemPrices.reduce((total, itemPrice, itemIndex, itemArray) =
         return total
     }
 });
+
 const mostExpensive = itemPrices.reduce((bigMoney, itemPrice) => {
     if (itemPrice < bigMoney) {
         return bigMoney
@@ -24,4 +27,49 @@ const mostExpensive = itemPrices.reduce((bigMoney, itemPrice) => {
 })
 
 
-console.log(mostExpensive)
+//console.log(mostExpensive)
+
+//REDUCE WITH DATA 
+
+const colorData = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
+
+const getColors = colorData.reduce((total, sample) => {
+    total.push(sample.color)
+    return total
+}, [])
+
+const getValues = colorData.reduce((total, sample) => {
+    total.push(sample.value)
+    return total
+}, [])
+
+console.log(getValues)
