d377b3876f5dee534254e0d91b77b84a4ce4747a
Will Neal <willwneal@gmail.com>
2022-07-06 18:07:45 -0400

    Complete queue example

--- cs/queues.js
+++ cs/queues.js
@@ -10,4 +10,28 @@ getInLine("Will")
 getInLine("Rudie")
 getInLine("Sal")
 
+
 console.log(queue)
+
+//who is next and last
+const nextInLine = () => queue[0]
+const lastInLine = () => queue[queue.length-1]
+
+// console.log(nextInLine() + " is first in line")
+// console.log(lastInLine() + " is last in line")
+
+//Call nextUp To move the line forward
+const nextUp = () => queue.shift()
+// nextUp()
+
+// console.log(queue)
+
+const cutTheLine = (person) => queue.unshift(person)
+// cutTheLine("Ben")
+
+// console.log(queue)
+
+const tiredOfWaiting = () => queue.pop()
+// tiredOfWaiting()
+
+// console.log(queue)
