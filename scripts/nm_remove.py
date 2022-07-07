45541857b8f133c9827686e9f3e6acaec185c13f
Will Neal <willwneal@gmail.com>
2022-07-07 16:32:10 -0400

    working python script

--- scripts/nm_remove.py
+++ scripts/nm_remove.py
@@ -1,5 +1,7 @@
 import os
 
 os.system("find . -name 'node_modules' -type d -prune")
+# '{}' is a placeholder which find replaces with the file path it found, while + tells find to append all the file paths to a single command, rather than running rm for each
+os.system("find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +")
 
-# find . -name 'node_modules' -type d -prune -exec rm -rf '{}' + WHERE '{}' is a placeholder which find replaces with the file path it found, while + tells find to append all the file paths to a single command, rather than running rm for each
+print("node_module pruning complete!!")
